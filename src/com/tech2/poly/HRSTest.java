b3a38ca5406001b0622fd2cad4253766da6a2b7a
hyunb915 <hyunb915@naver.com>
2020-08-19 15:33:25 +0900

    inter_step2

--- src/com/tech2/poly/HRSTest.java
+++ src/com/tech2/poly/HRSTest.java
@@ -1,13 +1,17 @@
 package com.tech2.poly;
 
-abstract class Employee{
+abstract class Employee {
 	String name;
 	int salary;
+
 	public abstract void calcSalary();
+
 	public abstract void calcBonus();
 }
 
-class Salesman extends Employee{
+class Salesman extends Employee {
+	int annual_sales;
+	
 	public void calcSalary() {
 		System.out.println("Salesman 급여 = 기본급 + 판매 수당");
 	}
@@ -17,44 +21,50 @@ class Salesman extends Employee{
 	}
 }
 
-class Consultant extends Employee{
+class Consultant extends Employee {
+	int num_project;
 	public void calcSalary() {
 		System.out.println("Consultant 급여 = 기본급+ 컨설팅 특별 수당");
 	}
+
 	public void calcBonus() {
 		System.out.println("Consultant 보너스 = 기본급 * 12 * 2");
 	}
 }
-abstract class Manager extends Employee{
+
+abstract class Manager extends Employee {
+	int num_team;
 	public void calcSalary() {
 		System.out.println("Manager 급여 = 기본급 + 팀 성과 수당");
 	}
 }
 
-class Director extends Manager{
+class Director extends Manager {
 	public void calcBonus() {
 		System.out.println("Director 보너스 = 기본급 * 12 * 6");
 	}
 }
 
 public class HRSTest {
-	public static void calcTax(Employee s) {
-//		System.out.println("소득세를 계산합니다.");
-		System.out.println("Salesman 비교 : "+(s instanceof Salesman));
-		if(s instanceof Salesman) {
-			System.out.println("Salesman입니다.");
+	public static void calcTax(Employee e) {
+		// System.out.println("소득세를 계산합니다.");
+		System.out.println("Salesman 비교 : " + (e instanceof Salesman));
+		if (e instanceof Salesman) {
+			Salesman s = (Salesman)e;
+			s.annual_sales = 6500000;
+			System.out.println("Salesman 입니다."+s.annual_sales);
+		} else if (e instanceof Manager) {
+			Manager m =(Manager)e;
+			m.num_team =5;
+			System.out.println("Manager입니다."+m.num_team);
+		} else if (e instanceof Consultant) {
+			Consultant c = (Consultant)e;
+			c.num_project = 35;
+			System.out.println("Consultant입니다."+c.num_project);
 		}
-		else if(s instanceof Manager) {
-			System.out.println("Manager입니다.");
-		}
-		else if(s instanceof Consultant) {
-			System.out.println("Consultant입니다.");
-		}
-		
-		
+
 	}
-	
-	
+
 	public static void main(String[] args) {
 		Salesman s = new Salesman();
 		Consultant c = new Consultant();
@@ -63,31 +73,31 @@ public class HRSTest {
 		arr[0] = s;
 		arr[1] = c;
 		arr[2] = d;
-//		for (Employee object : arr) {
-//			calcTax(object);
-//		}
-		
-		calcTax(s);
+		// for (Employee object : arr) {
+		// calcTax(object);
+		// }
+
+		calcTax(d);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}
-		
-//		s.calcBonus();
-//		c.calcBonus();
-//		d.calcBonus();
-//		System.out.println(s.toString());
-//		System.out.println(c.toString());
-//		System.out.println(d.toString());
-//		Salesman s2 = s;
-//		if(s.equals(s2)) {
-//			System.out.println("동일");
-//		}
-//		else {
-//			System.out.println("다르다");
-//		}
-		
-//		calcTax(c);
-//		calcTax(d);
+
+		// s.calcBonus();
+		// c.calcBonus();
+		// d.calcBonus();
+		// System.out.println(s.toString());
+		// System.out.println(c.toString());
+		// System.out.println(d.toString());
+		// Salesman s2 = s;
+		// if(s.equals(s2)) {
+		// System.out.println("동일");
+		// }
+		// else {
+		// System.out.println("다르다");
+		// }
+
+		// calcTax(c);
+		// calcTax(d);
 	}
-	
+
 }
--- src/com/tech3/inter_di1/HotAmericano.java
+++ src/com/tech3/inter_di1/HotAmericano.java
@@ -1,5 +1,6 @@
 package com.tech3.inter_di1;
 
 public class HotAmericano {
+	private HotAmericano ame;
 
 }
